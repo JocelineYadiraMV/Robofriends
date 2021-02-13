1f596602b4d83afa1454db57244e973f2a32a779
Joceline <jocelinemartinezvidal@MacBook-Pro-de-Joceline.local>
2021-02-13 17:53:56 -0600

    adding error alert

---
+++ src/components/ErrorBoundry.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class ErrorBoundry extends Component {
+	constructor(props){
+		super(props);
+		this.state = {
+			hasError: false
+		}
+	}
+
+	componentDidCatch(error, info){
+		this.setState({ hasError: true })
+	}
+
+	render(){
+		return this.state.hasError ? <h1 className='tc f2'>Ooops. Something went wrong!</h1> : this.props.children
+	}
+}
+
+export default ErrorBoundry;
